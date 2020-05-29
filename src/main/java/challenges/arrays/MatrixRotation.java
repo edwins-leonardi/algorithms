c1d93d5533347d560763e3710e3beef8403a98ea
Edwins Leonardi <eppadilh@amazon.com>
2020-05-29 19:23:31 +0100

    add print statement

--- src/main/java/challenges/arrays/MatrixRotation.java
+++ src/main/java/challenges/arrays/MatrixRotation.java
@@ -12,8 +12,11 @@ public class MatrixRotation {
 		int[][] matrix = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 },
 				{ 13, 14, 15, 16 } };
 
-		matrix = rotate(matrix, 4);
+		printMatrix(matrix);
 
+
+		rotate(matrix, 4);
+		System.out.println();
 		printMatrix(matrix);
 	}
 
@@ -42,33 +45,27 @@ public class MatrixRotation {
 
 	}
 
-	public static int[][] rotate(int[][] matrix, int n) {
-
-		for (int layer = 0; layer < matrix.length / 2; ++layer) {
+	public static void rotate(int[][] matrix, int n) {
+		for (int layer =0; layer < n/2; layer++){
 			int first = layer;
 			int last = n - 1 - layer;
-			for (int i = first; i < last; ++i) {
-
+			for(int i = first; i < last; ++i) {
 				int offset = i - first;
 
-				// from top to temp
 				int temp = matrix[first][i];
 
-				// from left to top
-				matrix[first][i] = matrix[last - offset][first];
+				//Rotate from left to top
+				matrix[first][i] = matrix[last-offset][first];
 
-				// from bottom to left
-				matrix[last - offset][first] = matrix[last][last - offset];
+				//rotate from bottom to left
+				matrix[last-offset][first] = matrix[last][last-offset];
 
-				// from right to bottom
-				matrix[last][last - offset] = matrix[i][last];
+				//rotate from right to bottom
+				matrix[last][last-offset] = matrix[i][last];
 
-				// from temp to right
+				//rotate from top to right
 				matrix[i][last] = temp;
 			}
 		}
-
-		return matrix;
 	}
-
 }
